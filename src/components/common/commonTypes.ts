2b2d16b0acccb19d3ce2a2e91b92c532021ea532
Vladyslav Ostroushko <v.ostroushko@zajno.com>
2023-05-05 13:34:29 +0300

    [1.1.0] Add preloader

---
+++ src/components/common/commonTypes.ts
@@ -0,0 +1,6 @@
+export enum CommonComponents {
+    Header = 'header',
+    Footer = 'footer',
+    MobileMenu = 'mobile-menu',
+    Preloader = 'preloader'
+}
---
+++ src/components/common/preloader/script.ts
@@ -0,0 +1,53 @@
+import Component, { ComponentConfig } from 'app/core/component';
+import gsap from 'gsap';
+import { CommonComponents } from '../commonTypes';
+import { LazyQueue } from 'app/components/lazy/lazyLoadComponent';
+
+class Preloader extends Component {
+    private _smoother: ScrollSmoother = null;
+    private _loaderIsActive: boolean = true;
+
+    constructor(el: HTMLElement, smoother?: ScrollSmoother) {
+        super({ el } as ComponentConfig);
+
+        this._smoother = smoother;
+    }
+
+    doSetup() {
+        this.setupAnimItems();
+    }
+
+    resize() {
+        // TODO
+    }
+
+    setupAnimItems() {
+        // TODO
+    }
+
+    public async show() {
+        document.body.classList.add('preloader-active');
+        this._smoother?.normalizer?.disable();
+        this._smoother?.paused();
+
+        gsap.to(this.element, { autoAlpha: 1 });
+    }
+
+    public async hide() {
+        document.body.classList.remove('preloader-active');
+        this._smoother?.normalizer?.enable();
+
+        gsap.to(this.element, { autoAlpha: 0 });
+    }
+}
+
+const preloader = new Preloader(document.getElementById(CommonComponents.Preloader));
+
+// If you want hide preloader after some priority.
+// LazyQueue.afterPriorityRun.on((prio) => prio === 1 && preloader.hide());
+
+// If you want hide preloader before some priority.
+// LazyQueue.afterPriorityRun.on((prio) => prio === 1 && preloader.hide());
+
+// If you want hide preloader after after lazy finished.
+LazyQueue.finished.on(() => preloader.hide());
